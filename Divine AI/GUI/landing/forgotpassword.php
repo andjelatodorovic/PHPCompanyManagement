119dfffc02e5041d20dafa3a6aa61f607404539c
Anđela Todorović <andjelatodorovicc@gmail.com>
2019-10-15 11:06:03 +0200

    Minor changes to forgotpassword.php

--- Divine AI/GUI/landing/forgotpassword.php
+++ Divine AI/GUI/landing/forgotpassword.php
@@ -1,6 +1,7 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
+    error_reporting(-1);
+    ini_set('display_errors', 'On');
+    set_error_handler("var_dump");
 session_start();
 // include all files //
 include_once '../../DAL/Database.php';
@@ -10,42 +11,56 @@ $db = $database->getConnection();
 
 if(isset($_POST['submit']))
 {
-    $mail=$_POST['email'];
-    $query = "SELECT * FROM radnik WHERE email = :mail";
+    $email=$_POST['email'];
+    $query = "SELECT * FROM radnik WHERE email = :email";
     $stmt = $db->prepare($query);
-    $stmt->execute(array(":mail" => $mail));
+    $stmt->execute(array(":email" => $email));
     $res= $stmt->fetch(PDO::FETCH_ASSOC);
     #print_r($res);
-
-    #$msg = "Your Password is :'".$otp."'";
-    $headers='From:andjellah@icloud.com';
-    $to=$res['email'];
-    $subject='Remind password';
-    //$m = mail($to,$subject,$msg,$headers);
+    $count = $stmt->rowCount();
     
-    
-    if($mail == $realemail){
+    if($count == 1 && !empty($res)){
         
-        $sql = "UPDATE admin SET password ='$otp' WHERE email='$mail'";
-        $m = mail($to,$subject,$msg,$headers);
+        $to=$res['email'];
+        $token=rand();
+        
+        $query1 = "INSERT INTO token (token,email) VALUES (:token, :email)";
+        $stmt1 = $db->prepare($query1);
+        $stmt1->execute(array(":token" => $token,":email" => $email));
+        $count1 = $stmt1->rowCount();
+        
+        
+        $subject='Password Recovery';
+        $from  = 'andjellah@icloud.com';
+        $headers = "From: " . $from . "\r\n";
+        $headers .= "Reply-To: ". $from . "\r\n";
+        $headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+        $message = '<html><body>';
+        $message .= '<span style="color:Red;font-size:18px;"> Resetujte lozinku</span> ';
+        $message .= '<table rules="all" border=1 style="border-color: green;" cellpadding="10" align="center">';
+        $message .= "<tr style='background: #eee;'><td><strong>Email:</strong> </td><td>" . strip_tags($res['email']) . "</td></tr>";
+        $message .= "<tr><td><strong>Token:</strong> </td><td>" . "$token'" . "</td></tr>";
+        $message .= "</table>";
+        $message .= "</body></html>";
+        $m=mail($to,$subject,$message,$headers);
+        var_dump($m);
         if($m)
         {
-            echo'Check your inbox in mail';
+            $msg='Check your mail inbox for Reset password ';
         }
         else
         {
-            echo'mail is not send';
+            $msg='Email not found please signup now!!';
         }
-        // echo $a = "hhh";
-        //else
-        //{
-        //echo'You entered mail id is not present';
-        
     }
-    
-    
+    else
+    {
+        $msg='Invalid Email';
+    }
 }
-?>
+    ?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
